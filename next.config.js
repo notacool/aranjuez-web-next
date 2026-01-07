ab1aa54ea774073b11f486e1cbdc5b73346a7e78
victoriacabirta <victoriacabirta@gmail.com>
2026-01-07 14:54:58 +0100

    Refactor code structure for improved readability and maintainability

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  plugins: [['styled-components', { ssr: true }]],
+  compiler: {
+    styledComponents: true,
+  },
   // typescript:{
   //   ignoreBuildErrors: true,
   // }
